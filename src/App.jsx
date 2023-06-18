bbdb2b6a5f5da5e5da8033e6fb4118ad6da759ce
Anandprabhu530 <anandprabhu530@gmail.com>
2023-06-18 12:56:30 +0530

    Initital commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+var index = 0;
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const votes = [0, 0, 0, 0, 0, 0, 0, 0];
+  const [selected, setSelected] = useState(0);
+  const [vote, setvote] = useState(votes);
+  const randomizer = () => {
+    var random_value = Math.floor(Math.random() * anecdotes.length);
+    while (anecdotes[random_value] === anecdotes[random_value - 1]) {
+      random_value = Math.floor(Math.random() * anecdotes.length);
+    }
+    index = random_value;
+    setSelected(random_value);
+  };
+  const voting = () => {
+    const arr = [...vote];
+    arr[index] += 1;
+    console.log(arr);
+    setvote(arr);
+  };
+  return (
+    <div>
+      {anecdotes[selected]} has {vote[index]} votes
+      <div>
+        <button onClick={randomizer}>Randon anecdotes</button>
+        <button onClick={voting}>Vote</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
