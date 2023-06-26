c29ddacb9ef95265808db202483f5ed16c6a82ac
Anandprabhu530 <anandprabhu530@gmail.com>
2023-06-26 20:01:41 +0530

    Updated till Exercise 2.8

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,68 @@ import { useState } from "react";
 const Display = ({ person }) => {
   return (
     <div>
-      <li>{person.name}</li>
+      <li>
+        {person.name}: {person.number}
+      </li>
     </div>
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Default User" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handlechange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handlechangeNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const adder = (event) => {
-    if (newName.length === 0) {
+    if (newName.length === 0 || newNumber.length === 0) {
       event.preventDefault();
+      alert("Invalid Name or Number");
       return;
+    }
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newName) {
+        alert(`${newName} already exists in the Phone book`);
+        event.preventDefault();
+        return;
+      }
     }
     event.preventDefault();
     const temp = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(temp));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Add a New</h2>
       <form onSubmit={adder}>
-        <input value={newName} onChange={handlechange} />
-        <button type="submit">add</button>
+        <div>
+          Name:
+          <input value={newName} onChange={handlechange} />
+        </div>
+        <div>
+          Phone No:
+          <input value={newNumber} onChange={handlechangeNumber} />
+        </div>
+        <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
       <div>
