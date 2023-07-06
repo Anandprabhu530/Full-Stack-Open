5cda8831e68b195eda88c0197c8818514d344eb2
Anandprabhu530 <anandprabhu530@gmail.com>
2023-07-06 15:34:33 +0530

    Exercise 2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Display = ({ person }) => {
   return (
@@ -13,17 +14,17 @@ const Display = ({ person }) => {
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newfilter, setNewFilter] = useState("");
   const [showfilter, setshowfilter] = useState(true);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  }, []);
   const handlechange = (event) => {
     setNewName(event.target.value);
   };
